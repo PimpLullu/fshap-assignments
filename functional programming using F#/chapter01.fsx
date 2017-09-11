8e0ea4c708f52ac323da7480dbe3ee49478865d6
Lullu <pimplullu@gmail.com>
2017-09-11 22:38:38 +0200

    chapter01

--- functional programming using F#/chapter01.fsx
+++ functional programming using F#/chapter01.fsx
@@ -31,19 +31,37 @@ let rec partialSum = function
 //correspond to the three cases of the above definition. Give an evaluations for F4.
 let rec fib = function
     | 0 | 1 -> 1
-    | n -> fib(n-1) + fib(n-1)
+    | n -> fib(n-1) + fib(n-2)
 
 //1.6 Declare a recursive function sum: int * int -> int, where
 //sum (m, n) = m + (m + 1) + (m + 2) + · · · + (m + (n − 1)) + (m + n)
 //for m ≥ 0 and n ≥ 0 . (Hint: use two clauses with (m,0) and (m,n) as patterns.)
 //Give the recursion formula corresponding to the declaration.
 
+let rec sum = function
+    | m, 0 -> 0
+    | m, n -> m + sum(m, (n-1))
+
 
 //1.7 Determine a type for each of the expressions:
-//(System.Math.PI, fact -1)     fact(fact 4)
-//power(System.Math.PI, fact 2)     (power, fact)
-
+//(System.Math.PI, fact -1) // fact -1 crashes the interactive console & float, int
+//fact(fact 4) = int = -775946240
+//power(System.Math.PI, fact 2) = float = 9.869604401
+//(power, fact) = val it : (float * int -> float) * (int -> int)
+let rec fact = function
+    | 0 -> 1
+    | n -> n * fact(n-1)
+let x = System.Math.PI, fact -1
+let y = fact(fact 4)
+let rec power = function
+    | (x,0) -> 1.0
+    | (x,n) -> x * power(x,n-1)
+let z = power(System.Math.PI, fact 2)
 
 //1.8 Consider the declarations:    let a = 5;;     let f a = a + 1;;   let g b = (f b) + a;;
 //Find the environment obtained from these declarations and write the evaluations of the expres-
 //sions f 3 and g 3.
+//envioment is a,f and g. f(3) = 4. g(3) = 9
+let a = 5;
+let f a = a + 1
+let g b = (f b) + a
